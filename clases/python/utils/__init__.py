554940686ab6395f8fd88ec3690ad9045ca4ea43
GbrlOl <gol197884266@gmail.com>
2025-03-01 22:46:18 -0300

    añadí la carpeta pytohn donde estarán todos los archivos python

---
+++ clases/python/utils/__init__.py
@@ -0,0 +1,5 @@
+from .mult import multiplicar
+from .saludar import decir_nombre
+from .suma import sumar
+
+COLOR = "red"
---
+++ clases/python/utils/suma.py
@@ -0,0 +1,3 @@
+def sumar(*args) -> int:
+    """Suma n valores de entrada"""
+    return sum(args)
