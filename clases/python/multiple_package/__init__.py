5d0ef17f3cab5673994b5eade0534b1f410c0318
GbrlOl <gol197884266@gmail.com>
2025-03-06 22:03:02 -0300

    Package para enseñar cómo operan los paquetes en python, esto es para la clase 15

---
+++ clases/python/multiple_package/__init__.py
@@ -0,0 +1,10 @@
+# Importamos el módulo de math
+from .math.math_module import suma, resta, multiplicacion, division
+
+# Importamos el módulo de math_input
+from .math_input.math_module_input import (
+    suma_in,
+    resta_in,
+    multiplicacion_in,
+    division_in,
+)
---
+++ clases/python/multiple_package/math_input/math_module_input.py
@@ -0,0 +1,23 @@
+def suma_in():
+    a = float(input("Ingresa el primer parámetro: "))
+    b = float(input("Ingresa el segundo parámetro: "))
+
+    return a + b
+
+
+def resta_in():
+    a = float(input("Ingresa el primer parámetro: "))
+    b = float(input("Ingresa el segundo parámetro: "))
+    return a - b
+
+
+def multiplicacion_in():
+    a = float(input("Ingresa el primer parámetro: "))
+    b = float(input("Ingresa el segundo parámetro: "))
+    return a * b
+
+
+def division_in():
+    a = float(input("Ingresa el primer parámetro: "))
+    b = float(input("Ingresa el segundo parámetro: "))
+    return a / b
